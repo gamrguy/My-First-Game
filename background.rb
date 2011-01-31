afe68022cc48cd71d28e4f58287e13f03ebb7cb4
Bill Davenport <bill@infoether.com>
2011-01-31 14:27:20 -0500

    changes I made at infoether

--- background.rb
+++ background.rb
@@ -13,23 +13,29 @@ class Background
     @shooting_star_y1 = 500 - (1  + rand(10))
     @shooting_star_y2 = 500 - (1  + rand(10))
     @shooting_star_y3 = 500 - (1  + rand(10))
-    @min_speed = 1
+    @min_speed = 6
     @max_speed = 10
-    @speed = @min_speed + rand(@max_speed - @min_speed)
+    @speed1 = @min_speed + rand(@max_speed - @min_speed)
+    @speed2 = @min_speed + rand(@max_speed - @min_speed)
+    @speed3 = @min_speed + rand(@max_speed - @min_speed)
+    
+    @angle1 = (-1 + rand(1))
+    @angle2 = (-1 + rand(1))
+    @angle3 = (-1 + rand(1))
   end
   
   def update
-    @shooting_star_x1 + (-1 + rand(1))
-    @shooting_star_x2 + (-1 + rand(1))
-    @shooting_star_x3 + (-1 + rand(1))
+    @shooting_star_x1 + @angle1
+    @shooting_star_x2 + @angle2
+    @shooting_star_x3 + @angle3
     
-    @shooting_star_y1 + (1 + rand(10))
-    @shooting_star_y2 + (1 + rand(10))
-    @shooting_star_y3 + (1 + rand(10))
+    @shooting_star_y1 + @speed1
+    @shooting_star_y2 + @speed2
+    @shooting_star_y3 + @speed3
     
-    reset1 if @shooting_star_y1 = 0
-    reset2 if @shooting_star_y2 = 0
-    reset3 if @shooting_star_y3 = 0
+    reset1 if @shooting_star_y1 == 0
+    reset2 if @shooting_star_y2 == 0
+    reset3 if @shooting_star_y3 == 0
   end
   
   def draw
@@ -42,20 +48,22 @@ class Background
   
   def reset1
     @shooting_star_x1 = 150
-    
     @shooting_star_y1 = 500
+    @speed1 = @min_speed + (@min_speed + rand(@max_speed - @min_speed))
   end
   
   def reset2
     @shooting_star_x2 = 150
-    
     @shooting_star_y2 = 500
+    @speed2 = @min_speed + (@min_speed + rand(@max_speed - @min_speed))
+    @angle2 = (-1 + rand(1))
   end
   
   def reset3
     @shooting_star_x3 = 150
-    
     @shooting_star_y3 = 500
+    @speed3 = @min_speed + (@min_speed + rand(@max_speed - @min_speed))
+    @angle3 = (-1 + rand(1))
   end
 
 end
